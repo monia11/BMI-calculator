28b9f3685d8ccd04a99874c8fe42c85a8b748d20
Monika Maleszka <maleszka.m93@gmail.com>
2020-02-11 15:23:33 +0100

    Add unit converter

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,95 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-export default function BmiCalc() {
+export default function BmiCalc(): JSX.Element {
   const [weight, setWeight] = useState(undefined);
   const [height, setHeight] = useState(undefined);
   const [bmi, setBmi] = useState(undefined);
+  const [weightPound, setWeightPound] = useState(undefined);
+  const [heightFoot, setHeightFoot] = useState(undefined);
 
-  const calculateBmi = e => {
+  const calculateBmi = (e: any): void => {
     e.preventDefault();
     if (weight && height) {
       setBmi(weight / (height * height));
     }
   };
+  const poundToKg = (e: any): void => {
+    setWeightPound(e.target.value);
+    setWeight(e.target.value * 0.45359237);
+  };
+
+  const footToM = (e: any): void => {
+    setHeightFoot(e.target.value);
+    setHeight(e.target.value * 0.3048);
+  };
+
+  const kgToPound = (e: any): void => {
+    setWeight(e.target.value);
+    setWeightPound(e.target.value * 2.20462262);
+  };
+
+  const mToFoot = (e: any): void => {
+    e.preventDefault();
+    setHeight(e.target.value);
+    setHeightFoot(e.target.value * 3.2808399);
+  };
 
   return (
     <div className="container">
+      <h1> BMI = {bmi && bmi !== 0 ? bmi.toFixed(2) : ' '} </h1>
       <form onSubmit={calculateBmi}>
-        <p>
-          Enter weight in <strong> kilograms</strong>
-        </p>
-        <input
-          type="number"
-          step="any"
-          min="0"
-          required
-          onChange={e => setWeight(e.target.value)}
-        />
-        <p>
-          Enter height in <strong> meters</strong>
-        </p>
-        <input
-          type="number"
-          step="any"
-          min="0"
-          required
-          onChange={e => setHeight(e.target.value)}
-        />
+        <div className="formPart">
+          <p>
+            Enter weight in <strong> kilograms (kg)</strong>
+          </p>
+          <input
+            type="number"
+            step="any"
+            min="0"
+            value={weight}
+            required
+            onChange={e => kgToPound(e)}
+          />
+          <p>
+            Enter weight in <strong> pounds (lb)</strong>
+          </p>
+          <input
+            type="number"
+            step="any"
+            min="0"
+            value={weightPound}
+            onChange={e => poundToKg(e)}
+          />
+        </div>
+        <div className="formPart">
+          <p>
+            Enter height in <strong> meters (m)</strong>
+          </p>
+          <input
+            type="number"
+            step="any"
+            min="0"
+            value={height}
+            required
+            onChange={e => mToFoot(e)}
+          />
+
+          <p>
+            Enter height in <strong> feets (ft)</strong>
+          </p>
+          <input
+            type="number"
+            step="any"
+            min="0"
+            value={heightFoot}
+            onChange={e => {
+              footToM(e);
+            }}
+          />
+        </div>
         <button type="submit">Calculate BMI</button>
       </form>
-
-      <h1> BMI = {bmi ? bmi.toFixed(2) : ' '} </h1>
     </div>
   );
 }
